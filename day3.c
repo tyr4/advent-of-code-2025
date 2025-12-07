0fe5e28d651ce57d749ecb664d43bc2dc6c67a22
tyr4 <wolfeveryday23@gmail.com>
2025-12-07 21:44:18 +0200

    updated day3 with part 2

--- day3.c
+++ day3.c
@@ -3,11 +3,59 @@
 #include <string.h>
 
 #define BUFFER_SIZE 1000
+#define NUM_OF_DIGITS 12 // can change this to 2 for part 1
+#define ARRAY_MAX 150
+
+void copyArray(int *src, int *dst, int size) {
+    for (int i = 0; i < size; i++) {
+        dst[i] = src[i];
+    }
+}
+
+long computeNum(char *nums, int *turnedOn, int size) {
+    long result = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (turnedOn[i]) {
+            result = result * 10 + (nums[i] - '0');
+        }
+    }
+
+    return result;
+}
+
+// checks which next digit turned on yields the max number
+long findMaxNewNumber(char *nums, int *src, int *dst, int size) {
+    int tempTurnedOn[size], maxTurnedOn[size];
+    long maxNum = 0;
+
+    copyArray(src, tempTurnedOn, size);
+
+    for (int i = 0; i < size; i++) {
+        // this digit is already turned on
+        if (tempTurnedOn[i]) continue;
+
+        tempTurnedOn[i] = 1;
+
+        long currentNum = computeNum(nums, tempTurnedOn, size);
+
+        if (maxNum < currentNum) {
+            maxNum = currentNum;
+            copyArray(tempTurnedOn, maxTurnedOn, size);
+        }
+
+        tempTurnedOn[i] = 0;
+    }
+
+    copyArray(maxTurnedOn, dst, size);
+    return maxNum;
+}
 
 int main() {
     FILE *file = fopen("../input.txt", "r");
     char buff[BUFFER_SIZE];
-    int rightMax = 0, leftMax = 0, sum = 0;
+    int len = 0;
+    long maxNum = 0, sum = 0;
 
     if (!file) {
         perror("file");
@@ -15,29 +63,21 @@ int main() {
     }
 
     while (fgets(buff, BUFFER_SIZE, file)) {
+        int turnedOn[ARRAY_MAX] = {0};
+
         buff[strcspn(buff, "\n")] = '\0';
-        // init them
-        leftMax = buff[0] - '0';
-        rightMax = buff[1] - '0';
+        len = strlen(buff);
 
-        for (int i = 2; i < strlen(buff); i++) {
-            int curr = buff[i] - '0';
-
-            if (rightMax > leftMax) {
-                leftMax = rightMax;
-                rightMax = curr;
-            }
-
-            if (rightMax < curr) {
-                rightMax = curr;
-            }
+        for (int i = 0; i < NUM_OF_DIGITS; i++) {
+            maxNum = findMaxNewNumber(buff, turnedOn, turnedOn, len);
+            printf("%ld\n", maxNum);
         }
 
-        sum += leftMax * 10 + rightMax;
-        printf("%d%d\n", leftMax, rightMax);
+        sum += maxNum;
+        printf("\n");
     }
 
-    printf("sum %d", sum);
+    printf("sum %ld", sum);
 
     fclose(file);
     return 0;
